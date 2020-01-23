da2d0c46c2a445184a3f25434db614657f59b766
Akash G Krishnan <krishnanag1996@gmail.com>
2020-01-23 23:30:40 +0530

    positive cases handled for the demo bank

--- DB/app.py
+++ DB/app.py
@@ -1,15 +1,31 @@
-from magic import addAcount, addNewUser
+from user import addAcount, withdraw_money, deposit_money, get_bank_balance
 
 print("1) Register an existing acount: ")
-print("2) Create an acount: ")
-print("3) Check Balance: ")
-print("4) Withdraw money: ")
+print("2) Check Balance: ")
+print("3) Withdraw money: ")
+print("4) Deposit money")
 choice = int(input("Choice: "))
 
 if choice == 1:
-    addAcount()
+    name = input("Enter name: ")
+    acount_no = input("Enter acount_no: ")
+    pin = input("Enter Age: ")
+    addAcount(name, acount_no, pin)
+
 elif choice == 2:
-    addNewUser()
+    get_bank_balance()
+
+elif  choice == 3:
+    amount = int(input("Enter the amount you want to withdraw: "))
+    withdraw_money(amount)
+    
+elif choice == 4:
+    amount = int(input("How muc would you like to deposit to your acount: "))
+    deposit_money(amount)
+
+else: 
+    print("System not trained for this kind of B.S")
+
 
 
 
--- DB/db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("Bank.db")
-c = conn.cursor()
-c.execute('''CREATE TABLE CUSTOMER_DTL
-(name text, acount_no integer, bank_balance integer, pin integer)
-''')
-conn.commit()
-conn.close()
-
